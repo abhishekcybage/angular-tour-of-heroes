18d1e3f7b98ab5b28e113a36d0450ac4c3a98e02
Abhishek Kumar <Abhishek.Kumar@capita.co.uk>
2019-08-06 19:06:11 +0530

    Heroes component, Hero Class, and mock-hero list added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+		{ id: 11, name: 'Abhishek' },
+		{ id: 12, name: 'Pratik' },
+		{ id: 13, name: 'Rosina' },
+		{ id: 14, name: 'Sneha' },
+		{ id: 15, name: 'Rohit' },
+		{ id: 16, name: 'Prakash' },
+		{ id: 17, name: 'Pratap' },
+		{ id: 18, name: 'Sahil' },
+		{ id: 19, name: 'Roshany' },
+		{ id: 20, name: 'Jyoti' }
+];
